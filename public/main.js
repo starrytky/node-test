31f18eb6eea8dadbdd5914ccbeffff1b209cfeaa
starrytky <starrytky@qq.com>
2021-11-15 23:00:08 +0800

    使用ajax获取html，css，js，xml，json

---
+++ public/main.js
@@ -0,0 +1,79 @@
+console.log("运行了这一行");
+getCss.onclick = function () {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/style.css');
+  request.onreadystatechange = () => {
+    if (request.readyState === 4){
+      if(request.status >=200 && request.status < 300){
+        const style = document.createElement('style');
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      }else {
+        alert('下载失败')
+      }
+    }
+  }
+  request.onload = function(){
+
+    console.log('成了')
+  }
+  request.onerror = function(){
+    console.log('白了')
+  }
+  request.send();
+}
+
+getJS.onclick = function() {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/2.js');
+  request.onload = function(){
+    const script = document.createElement('script');
+    script.innerHTML = request.response;
+    document.body.appendChild(script);
+    console.log('成了')
+  }
+  request.onerror = function(){
+    console.log('白了')
+  }
+  request.send();
+}
+
+getHTML.onclick = function(){
+  const request = new XMLHttpRequest();
+  request.open('GET', '/3.html');
+  request.onload = function() {
+    const div = document.createElement('div')
+    div.innerHTML = request.response;
+    document.body.appendChild(div);
+    console.log('成了')
+  }
+  request.onerror = function() {
+    console.log("败了")
+  }
+  request.send();
+}
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/4.xml')
+  request.onreadystatechange = ()=>{
+    if(request.readyState === 4 && request.status === 200){
+      const dom = request.responseXML;
+      console.log(dom.getElementsByTagName('warning')[0].textContent.trim())      
+    }
+  }
+  request.send();
+}
+
+getJSON.onclick = function(){
+  const request = new XMLHttpRequest();
+  request.open("GET", '/5.json');
+  console.log(12121)
+  request.onreadystatechange = ()=>{
+    if(request.readyState === 4 && request.status === 200) {
+      const json = JSON.parse(request.response)
+      console.log(json)
+    }
+  }
+  request.send();
+}
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -21,12 +21,47 @@ var server = http.createServer(function(request, response){
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  if(path === '/'){
+  if (path === '/') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`二哈`)
     response.end()
-  } else if(path === '/x'){
+  } else if (path === '/index.html') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(fs.readFileSync('public/index.html'));
+    response.end()
+  } else if (path === '/main.js') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.write(fs.readFileSync('public/main.js'));
+    response.end();
+  } else if (path === '/2.js') {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.write(fs.readFileSync('public/2.js'));
+    response.end()
+  } else if (path === '/3.html') {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(fs.readFileSync('public/3.html'));
+    response.end()
+  } else if (path === '/4.xml') {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/xml;charset=utf-8')
+    response.write(fs.readFileSync('public/4.xml'));
+    response.end()
+  } else if(path === '/5.json') {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json;charset=utf-8')
+    response.write(fs.readFileSync('public/5.json'));
+    response.end();
+  }else if (path === '/x') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    response.write(`body{color: red;}`)
+    response.end()
+  } else if (path === '/style.css') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color: red;}`)
